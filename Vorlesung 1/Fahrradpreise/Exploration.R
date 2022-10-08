1a02fb0ba1f49cf8ea87044e540aa53f7335e01a
Meike Wocken <m.wocken@gmx.de>
2022-10-08 10:36:48 +0200

    Adding solution

--- Vorlesung 1/Fahrradpreise/Exploration.R
+++ Vorlesung 1/Fahrradpreise/Exploration.R
@@ -1,5 +1,6 @@
-library(tidyverse)
+#install.packages("tidyverse")
 
+library(tidyverse)
 mydat <- read_csv2("Vorlesung 1/Fahrradpreise/fahrrad_preise.csv")
 
 mydat %>%
@@ -16,3 +17,10 @@ ggplot(data = mydat) +
   geom_density(
     mapping = aes(x = preis, y = ..density..))
 
+# Lösung 1: BASE R
+mydat.df <- as.data.frame(mydat)
+boxplot(mydat.df$preis ~ mydat.df$farbe)
+
+# Lösung 2: tidyverse
+ggplot(data = mydat) + 
+  geom_boxplot(mapping =aes(x=farbe, y=preis))
